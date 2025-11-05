3b415bd3713e6aa2b0fc5b357f7ffdb5e32cf0f0
Genma-shi <baisalovdastan150505@gmail.com>
2025-11-05 18:39:17 +0600

    Оставлен только frontend в ветке main_frontend

--- departments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import DepartmentListCreateView, DepartmentDetailView
-
-urlpatterns = [
-    path('', DepartmentListCreateView.as_view(), name='department-list-create'),
-    path('<int:pk>/', DepartmentDetailView.as_view(), name='department-detail'),
-]
--- specializations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SpecializationListCreateView, SpecializationDetailView
-
-urlpatterns = [
-    path('', SpecializationListCreateView.as_view(), name='specialization-list-create'),
-    path('<int:pk>/', SpecializationDetailView.as_view(), name='specialization-detail'),
-]
