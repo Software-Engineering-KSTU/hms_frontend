39b7b2231b8f9fa10787d604fc31e5d61211d518
erikomaraliev <theslyfox43@gmail.com>
2025-12-08 05:31:09 +0600

    added navigation to 404 and 500 screen

--- lib/navigation/Navigation.dart
+++ lib/navigation/Navigation.dart
@@ -4,6 +4,8 @@ import 'package:hmsweb/%20clinic_contact_info/ui/ContactsModel.dart';
 import 'package:hmsweb/%20clinic_contact_info/ui/ContactsScreen.dart';
 import 'package:hmsweb/base/BaseScreenModel.dart';
 import 'package:hmsweb/base/view/CustomAppBar.dart';
+import 'package:hmsweb/errorpage/ui/Error500Page.dart';
+import 'package:hmsweb/errorpage/ui/ErrorScreenModel.dart';
 import 'package:hmsweb/home/ui/HomeModel.dart';
 import 'package:hmsweb/home/ui/HomeScreen.dart';
 import 'package:hmsweb/patient_appointment/dashboard/ui/doctor_list/DoctorListScreen.dart';
@@ -15,6 +17,7 @@ import 'package:hmsweb/auth/AuthModel.dart';
 
 import '../doctor_appointment/dashboard/ui/DoctorDashboardScreen.dart';
 import '../doctor_appointment/dashboard/ui/DoctorDashboardScreenModel.dart';
+import '../errorpage/ui/Error404Page.dart';
 import '../patient_appointment/dashboard/ui/PatientDashboardScreen.dart';
 import '../patient_appointment/dashboard/ui/PatientDashboardScreenModel.dart';
 
@@ -26,14 +29,10 @@ GoRoute buildRoute<T extends BaseScreenModel>({
   return GoRoute(
     path: path,
     builder: (context, state) {
-
       final model = createModel(state);
       model.initialize();
 
-      return ChangeNotifierProvider.value(
-        value: model,
-        child: screen,
-      );
+      return ChangeNotifierProvider.value(value: model, child: screen);
     },
   );
 }
@@ -43,16 +42,21 @@ final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 final GoRouter router = GoRouter(
   navigatorKey: rootNavigatorKey,
   initialLocation: '/',
+  errorPageBuilder: (context, state) {
+    return const MaterialPage(key: ValueKey('error'), child: Error404Page());
+  },
   routes: [
     buildRoute(
-        path: '/doctor/dashboard',
-        screen: DoctorDashboardScreen(),
-        createModel: (state) => DoctorDashboardScreenModel()),
+      path: '/doctor/dashboard',
+      screen: DoctorDashboardScreen(),
+      createModel: (state) => DoctorDashboardScreenModel(),
+    ),
 
     buildRoute(
-        path: '/patient/doctors',
-        screen: DoctorListScreen(),
-        createModel: (state) => DoctorListScreenModel()),
+      path: '/patient/doctors',
+      screen: DoctorListScreen(),
+      createModel: (state) => DoctorListScreenModel(),
+    ),
 
     GoRoute(
       path: '/patient/dashboard',
@@ -74,30 +78,34 @@ final GoRouter router = GoRouter(
       screen: LoginScreen(),
       createModel: (sate) => AuthModel(),
     ),
+
     buildRoute(
       path: '/registration',
       screen: RegistrationScreen(),
       createModel: (state) => AuthModel(),
     ),
 
+    buildRoute(
+      path: '/oops',
+      screen: Error500Page(),
+      createModel: (state) => ErrorScreenModel(),
+    ),
+
     ShellRoute(
       builder: (context, state, child) {
-        return Scaffold(
-          appBar: const CustomAppBar(),
-          body: child,
-        );
+        return Scaffold(appBar: const CustomAppBar(), body: child);
       },
       routes: [
         buildRoute(
-            path: '/',
-            screen: HomeScreen(),
-            createModel: (state) => HomeModel()
+          path: '/',
+          screen: HomeScreen(),
+          createModel: (state) => HomeModel(),
         ),
 
         buildRoute(
-            path: '/contacts',
-            screen: ContactsScreen(),
-            createModel: (state) => ContactsModel()
+          path: '/contacts',
+          screen: ContactsScreen(),
+          createModel: (state) => ContactsModel(),
         ),
       ],
     ),
